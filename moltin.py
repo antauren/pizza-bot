04e49d0346747d60aadbda2073e4887647aabc51
Anatoly <antauren@yandex.ru>
2020-12-02 22:47:10 +0300

    add func create_flow

--- moltin.py
+++ moltin.py
@@ -151,3 +151,25 @@ def create_category(access_token, name: str, slug: str, description: str, status
     response.raise_for_status()
 
     return response.json()
+
+
+def create_flow(access_token: str, name: str, slug: str, description: str, enabled: bool = True) -> dict:
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        'data': {
+            'type': 'flow',
+            'name': name,
+            'slug': slug,
+            'description': description,
+            'enabled': enabled
+        }
+    }
+
+    response = requests.post('https://api.moltin.com/v2/flows', headers=headers, json=data)
+    response.raise_for_status()
+
+    return response.json()
