0ebf966bf371549c1373568e132d1abd8d32e494
Anatoly <antauren@yandex.ru>
2020-12-02 22:27:12 +0300

    add func create_category

--- moltin.py
+++ moltin.py
@@ -129,3 +129,25 @@ def create_main_image_relationship(access_token, product_id: str, image_id: str)
     response.raise_for_status()
 
     return response.json()
+
+
+def create_category(access_token, name: str, slug: str, description: str, status: str = 'live') -> dict:
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        'data': {
+            'type': 'category',
+            'name': name,
+            'slug': slug,
+            'description': description,
+            'status': status
+        }
+    }
+
+    response = requests.post('https://api.moltin.com/v2/categories', headers=headers, json=data)
+    response.raise_for_status()
+
+    return response.json()
