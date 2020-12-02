eb3c275a591a8cf87cd5c19236fed8e2265fcd9c
Anatoly <antauren@yandex.ru>
2020-12-02 21:19:17 +0300

    add func create_file_from_url

--- moltin.py
+++ moltin.py
@@ -95,3 +95,21 @@ def create_category_relationship(access_token: str, product_id: str, category_id
     response.raise_for_status()
 
     return response.json()
+
+
+def create_file_from_url(access_token: str, url: str, filename: str) -> dict:
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+    }
+
+    file = requests.get(url)
+
+    files = {
+        'file': (filename, file.content),
+        'public': (None, 'true'),
+    }
+
+    response = requests.post('https://api.moltin.com/v2/files', headers=headers, files=files)
+    response.raise_for_status()
+
+    return response.json()
