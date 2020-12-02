8f2fa7943c84ecb06435bcf43b085d2f0ce7a408
Anatoly <antauren@yandex.ru>
2020-12-02 19:50:30 +0300

    add func create_product

--- moltin.py
+++ moltin.py
@@ -36,3 +36,46 @@ def _get_access_token(client_id: str, client_secret: str) -> dict:
     response.raise_for_status()
 
     return response.json()
+
+
+def create_product(access_token: str,
+                   name: str,
+                   slug: str,
+                   sku: str,
+                   description: str,
+                   price: int,
+                   currency: str = 'RUB',
+                   includes_tax: bool = True,
+                   status: str = 'live',
+                   manage_stock: bool = True,
+                   commodity_type: str = 'physical',
+                   ) -> dict:
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        'data': {
+            'type': 'product',
+            'name': name,
+            'slug': slug,
+            'sku': sku,
+            'description': description,
+            'manage_stock': manage_stock,
+            'price': [
+                {
+                    'amount': price,
+                    'currency': currency,
+                    'includes_tax': includes_tax,
+                }
+            ],
+            'status': status,
+            'commodity_type': commodity_type
+        }
+    }
+
+    response = requests.post('https://api.moltin.com/v2/products', headers=headers, json=data)
+    response.raise_for_status()
+
+    return response.json()
