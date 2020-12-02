7345941a7b313ab595cab1792c128731a9832a29
Anatoly <antauren@yandex.ru>
2020-12-03 02:56:51 +0300

    move create_entry

--- moltin.py
+++ moltin.py
@@ -1,19 +1,2 @@
-# https://documentation.elasticpath.com/commerce-cloud/docs/api/
 
 
-import requests
-
-
-def create_entry(access_token: str, flow_slug: str, entry: dict) -> dict:
-    headers = {
-        'Authorization': 'Bearer {}'.format(access_token),
-        'Content-Type': 'application/json',
-    }
-
-    data = {'data': {'type': 'entry', }}
-    data['data'].update(entry)
-
-    response = requests.post('https://api.moltin.com/v2/flows/{}/entries'.format(flow_slug), headers=headers, json=data)
-    response.raise_for_status()
-
-    return response.json()
