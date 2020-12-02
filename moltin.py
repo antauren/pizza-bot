4671e316643a90db5784ac3c762aa20a85c13a1b
Anatoly <antauren@yandex.ru>
2020-12-03 02:30:47 +0300

    move create_product

--- moltin.py
+++ moltin.py
@@ -4,49 +4,6 @@
 import requests
 
 
-def create_product(access_token: str,
-                   name: str,
-                   slug: str,
-                   sku: str,
-                   description: str,
-                   price: int,
-                   currency: str = 'RUB',
-                   includes_tax: bool = True,
-                   status: str = 'live',
-                   manage_stock: bool = False,
-                   commodity_type: str = 'physical',
-                   ) -> dict:
-    headers = {
-        'Authorization': 'Bearer {}'.format(access_token),
-        'Content-Type': 'application/json',
-    }
-
-    data = {
-        'data': {
-            'type': 'product',
-            'name': name,
-            'slug': slug,
-            'sku': sku,
-            'description': description,
-            'manage_stock': manage_stock,
-            'price': [
-                {
-                    'amount': price,
-                    'currency': currency,
-                    'includes_tax': includes_tax,
-                }
-            ],
-            'status': status,
-            'commodity_type': commodity_type
-        }
-    }
-
-    response = requests.post('https://api.moltin.com/v2/products', headers=headers, json=data)
-    response.raise_for_status()
-
-    return response.json()
-
-
 def create_category_relationship(access_token: str, product_id: str, category_id: str) -> dict:
     headers = {
         'Authorization': 'Bearer {}'.format(access_token),
