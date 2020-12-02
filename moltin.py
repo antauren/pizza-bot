b8ae42a418c3d5221b682267931e322488c0796f
Anatoly <antauren@yandex.ru>
2020-12-02 20:37:46 +0300

    add func create_category_relationship

--- moltin.py
+++ moltin.py
@@ -79,3 +79,19 @@ def create_product(access_token: str,
     response.raise_for_status()
 
     return response.json()
+
+
+def create_category_relationship(access_token: str, product_id: str, category_id: str) -> dict:
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    data = {'data': [{'type': 'category', 'id': category_id}]}
+
+    response = requests.post('https://api.moltin.com/v2/products/{}/relationships/categories'.format(product_id),
+                             headers=headers,
+                             json=data)
+    response.raise_for_status()
+
+    return response.json()
