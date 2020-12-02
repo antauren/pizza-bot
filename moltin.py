ed2f7aa33c8736b82fdc3e9d30a92b301b4116cc
Anatoly <antauren@yandex.ru>
2020-12-03 00:19:29 +0300

    add func create_entry

--- moltin.py
+++ moltin.py
@@ -224,3 +224,18 @@ def create_field(access_token: str,
     response.raise_for_status()
 
     return response.json()
+
+
+def create_entry(access_token: str, flow_slug: str, entry: dict) -> dict:
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    data = {'data': {'type': 'entry', }}
+    data['data'].update(entry)
+
+    response = requests.post('https://api.moltin.com/v2/flows/{}/entries'.format(flow_slug), headers=headers, json=data)
+    response.raise_for_status()
+
+    return response.json()
