d5e088e5f7a78176f65ed5140f5f9d600022cae1
Anatoly <antauren@yandex.ru>
2020-12-02 21:36:24 +0300

    add func create_main_image_relationship

--- moltin.py
+++ moltin.py
@@ -113,3 +113,19 @@ def create_file_from_url(access_token: str, url: str, filename: str) -> dict:
     response.raise_for_status()
 
     return response.json()
+
+
+def create_main_image_relationship(access_token, product_id: str, image_id: str) -> dict:
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    data = {'data': {'type': 'main_image', 'id': image_id}}
+
+    response = requests.post('https://api.moltin.com/v2/products/{}/relationships/main-image'.format(product_id),
+                             headers=headers,
+                             json=data)
+    response.raise_for_status()
+
+    return response.json()
