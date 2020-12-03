2856ac99ef762e1d12326343c53b2760bf57f850
Anatoly <antauren@yandex.ru>
2020-12-03 04:09:41 +0300

    add func get_cart_items

--- moltin/carts.py
+++ moltin/carts.py
@@ -3,6 +3,15 @@
 import requests
 
 
+def get_cart_items(access_token: str, user: str) -> dict:
+    headers = {'Authorization': 'Bearer {}'.format(access_token)}
+
+    response = requests.get('https://api.moltin.com/v2/carts/{}/items'.format(user), headers=headers)
+    response.raise_for_status()
+
+    return response.json()
+
+
 def add_product_to_cart(access_token: str, customer: str, product_id: str, quantity: int) -> dict:
     headers = {
         'Authorization': 'Bearer {}'.format(access_token),
