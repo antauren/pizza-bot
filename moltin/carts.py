498d9bddbec9c6ab727b83fc3980d996788ca17d
Anatoly <antauren@yandex.ru>
2020-12-03 04:07:48 +0300

    add func remove_cart_item

--- moltin/carts.py
+++ moltin/carts.py
@@ -21,3 +21,12 @@ def add_product_to_cart(access_token: str, customer: str, product_id: str, quant
     response.raise_for_status()
 
     return response.json()
+
+
+def remove_cart_item(access_token: str, user: str, product_id: str) -> dict:
+    headers = {'Authorization': 'Bearer {}'.format(access_token)}
+
+    response = requests.delete('https://api.moltin.com/v2/carts/{}/items/{}'.format(user, product_id), headers=headers)
+    response.raise_for_status()
+
+    return response.json()
